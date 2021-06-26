b98995265f85e668f5bb9d3646d0576bdafec114
Adrien Debord <adrien.debord@gmail.com>
2021-06-26 17:19:33 +0200

    good key

--- keyboards/gboards/k/gergoplex/keymaps/A-Debord/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/A-Debord/keymap.c
@@ -88,7 +88,7 @@ MT(MOD_RSFT,KC_PERC), XXXXXXX, XXXXXXX, XXXXXXX,TD_LBRC_LES, TD_RBRC_GRT, KC_QUE
         //┌────────┬────────┬────────┬────────┬────────┐    ┌────────┬────────┬────────┬────────┬────────┐
            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8, 	KC_9, 	 KC_0,
         //├────────┼────────┼────────┼────────┼────────┤    ├────────┼────────┼────────┼────────┼────────┤
-           MOD_LCTL, KC_HOME, KC_PGUP, KC_PGDN, KC_END,      KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, MOD_RCTL,
+           KC_LCTL, KC_HOME, KC_PGUP, KC_PGDN, KC_END,      KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_RCTL,
         //├────────┼────────┼────────┼────────┼────────┤    ├────────┼────────┼────────┼────────┼────────┤
 MT(MOD_RSFT,KC_F1), KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   MT(MOD_RSFT,KC_F10),
         //└─┬──────┴─┬──────┴┬────┴───┬───────┴────────┘    └────────┴───────┬┴───────┬┴───────┬┴───────┬┘
